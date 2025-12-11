6188ffab18ea839764429858570796badd8cdfea
Legends-Land <caandrews18@gmail.com>
2025-12-10 21:27:16 -0500

    Version 4

--- change.js
+++ change.js
@@ -3,4 +3,5 @@ console.log('Hello, World!');
 <body>
     <div>Slight change to see this file in the working are. Right before the staging area. </div>
     <div> Version 3 </div>
+    <div> Version 4</div>
 </body>
--- src/index.js
+++ src/index.js
@@ -3,4 +3,5 @@ console.log('Version 3 update');
 
 <body>
     <div>We made come changes and we will use git add . to stage then git committ</div>
+    <div>Version 4 changes</div>
 </body>
