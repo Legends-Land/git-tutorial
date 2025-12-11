ccbba69e64cba822416c16839129e5da723d466f
Legends-Land <caandrews18@gmail.com>
2025-12-10 21:53:55 -0500

    Added more to the divs

--- change.js
+++ change.js
@@ -4,5 +4,5 @@ console.log('Hello, World!');
     <div>Slight change to see this file in the working are. Right before the staging area. </div>
     <div> Version 3 </div>
     <div> Version 4</div>
-    <div> Version 5 Dec 10 21:31</div>
+    <div> Version 5 Dec 10 21:31 we just added more to this line at 21:50</div>
 </body>
--- src/index.js
+++ src/index.js
@@ -4,6 +4,6 @@ console.log('Hello, World!');
 <body>
     <div>We made come changes and we will use git add . to stage then git committ</div>
     <div>Version 4 changes</div>
-    <div>Version 5 Dec 10 21:31 changes</div>
+    <div>Version 5 Dec 10 21:31 changes We added more changes. When go and the heart dont</div>
     
 </body>
